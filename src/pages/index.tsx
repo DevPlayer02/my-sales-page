c44ea47d972f71c6b2ff831f94f17073b6c03adb
DevPlayer02 <contato.giovanicf@gmail.com>
2025-07-29 22:21:05 -0300

    feat: Adding and styling section two

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { Banner } from "@/components/Banner";
 import { Header } from "@/components/Header";
+import { SectionTwo } from "@/components/SectionTwo";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Banner />
+      <SectionTwo />
     </>
   );
 }
