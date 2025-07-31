7075c8bcfc3fc2c8b1ebef762b1076adb34c280e
DevPlayer02 <contato.giovanicf@gmail.com>
2025-07-31 19:17:31 -0300

    feat: Adding e styling moving bar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Banner } from "@/components/Banner";
 import { Header } from "@/components/Header";
+import { MovingBars } from "@/components/MovingBars";
 import { SectionTwo } from "@/components/SectionTwo";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <Banner />
       <SectionTwo />
+      <MovingBars />
     </>
   );
 }
